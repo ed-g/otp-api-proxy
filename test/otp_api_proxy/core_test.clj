e0da94c661423c179f3a791a9986801308419498
ed-g <edward.groth@gmail.com>
2015-04-09 18:36:01 -0700

    test dir

---
+++ test/otp_api_proxy/core_test.clj
@@ -0,0 +1,7 @@
+(ns otp-api-proxy.core-test
+  (:require [clojure.test :refer :all]
+            [otp-api-proxy.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 1 1))))
